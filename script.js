3d81b15d281a84e6efbd4bf4031e38b62a230682
Sophia De La Rosa <sophial.delarosa@gmail.com>
2022-05-18 19:53:17 -0400

    Version 1: Files created. HTML and CSS mostly done. JavaScript just started. Hide/show next question in progress.

---
+++ script.js
@@ -0,0 +1,49 @@
+//var generateBtn = document.querySelector("#generate");
+
+//generateBtn.addEventListener("click", generateScore);
+
+//Questions
+
+//Commonly used data types do NOT include
+//strings 
+//booleans 
+//alerts --correct
+//numbers
+
+//The condition of an if/else statement is enclosed within
+//quotes
+//curly brackets 
+//parentheses 
+//square brackets
+
+//arrays in JavaScript can be used to store
+//numbers and strings
+//other arrays
+//booleans
+//all of the above --correct
+
+//string values must be enclosed within ___ when being assigned to variables
+//commas
+//curly brackets
+//quotes
+//parentheses
+
+//A very useful tool during development for debugging and printing content to the debugger is
+//JavaScript
+//terminal/bash
+//for loops
+//console.log --correct
+
+//Declaring the elements
+
+const start_btn = document.getElementById('start-btn button');
+const next_btn = document.getElementById('next-btn button');
+
+const starter_box = document.getElementById('starter-box');
+const quiz_box = document.getElementById('quiz-box');
+const result_box = document.getElementById('result-box');
+const highscore_box = document.getElementById('highscore-box');
+
+// If start button is clicked
+
+start_btn.onclick = ()=>[]
