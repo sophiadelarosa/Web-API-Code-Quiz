dcea64fd4771e2c695005e7f15abca37ceb474b7
Sophia De La Rosa <sophial.delarosa@gmail.com>
2022-05-19 21:49:17 -0400

    Version 2: Questions work and cycle through. No scores, results or highscores yet.

--- script.js
+++ script.js
@@ -1,49 +1,73 @@
-//var generateBtn = document.querySelector("#generate");
-
-//generateBtn.addEventListener("click", generateScore);
-
-//Questions
-
-//Commonly used data types do NOT include
-//strings 
-//booleans 
-//alerts --correct
-//numbers
-
-//The condition of an if/else statement is enclosed within
-//quotes
-//curly brackets 
-//parentheses 
-//square brackets
-
-//arrays in JavaScript can be used to store
-//numbers and strings
-//other arrays
-//booleans
-//all of the above --correct
-
-//string values must be enclosed within ___ when being assigned to variables
-//commas
-//curly brackets
-//quotes
-//parentheses
-
-//A very useful tool during development for debugging and printing content to the debugger is
-//JavaScript
-//terminal/bash
-//for loops
-//console.log --correct
-
 //Declaring the elements
+let start_btn = document.getElementById("start-btn");
 
-const start_btn = document.getElementById('start-btn button');
-const next_btn = document.getElementById('next-btn button');
+let box = document.getElementById("box");
+let result_box = document.getElementById("result-box");
+let highscore_box = document.getElementById("highscore-box");
 
-const starter_box = document.getElementById('starter-box');
-const quiz_box = document.getElementById('quiz-box');
-const result_box = document.getElementById('result-box');
-const highscore_box = document.getElementById('highscore-box');
+//question iterator
+let qi = -1;
 
-// If start button is clicked
+//creating an array for questions and stuff
+let questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        options: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
+        answer: "C",
+    },
+    {
+        question: "The condition of an if/else statement is enclosed within:",
+        options: ["A. Quotes", "B. Curly Brackets", "C. Parantheses", "D. Square Brackets"],
+        answer: "B",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        options: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
+        answer: "D",
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables",
+        options: ["A. Commas", "B. Curly brackets", "C. Quotes", "D. Parentheses"],
+        answer: "C",
+    },
+    {
+        question: "A very useful tool during development for debugging and printing content to the debugger is:",
+        options: ["A. JavaScript", "B. Terminal/Bash", "C. console.log", "D. for loops"],
+        answer: "C",
+    },
+]
 
-start_btn.onclick = ()=>[]
+//If start button is clicked
+start_btn.addEventListener("click", createQuiz);
+
+//start the quiz
+function createQuiz() {
+    questionLooper();
+    console.log("quiz time!");
+}
+
+//function to end the quiz
+function endQuiz() {
+    box.innerHTML = `<h1>Quiz Complete</h1>`;
+}
+
+//function to loop through the questions
+function questionLooper() {
+    qi++
+    if (qi >= questions.length) {
+        endQuiz();
+        return;
+    }
+
+    //Puts quiz questions in the main box on the page
+    let { question, options, answer } = questions[qi];
+    box.innerHTML = `<h1>${question}</h1>`;
+    box.innerHTML += `<button value= "A" onclick="checkAnswer(event, qi)"> ${options[0]} </button>`;
+    box.innerHTML += `<button value= "B" onclick="checkAnswer(event, qi)"> ${options[1]} </button>`;
+    box.innerHTML += `<button value= "C" onclick="checkAnswer(event, qi)"> ${options[2]} </button>`;
+    box.innerHTML += `<button value= "D" onclick="checkAnswer(event, qi)"> ${options[3]} </button>`;
+}
+
+function checkAnswer() {
+    questionLooper();
+}
